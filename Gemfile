d0ff611de40052b19fe4aa50490888e2fcfb0e51
EC2 Default User <ec2-user@ip-172-31-41-159.us-east-2.compute.internal>
2018-05-21 15:10:32 +0000

    bootstrap

--- Gemfile
+++ Gemfile
@@ -11,6 +11,8 @@ gem 'rails', '~> 5.1.6'
 # Use sqlite3 as the database for Active Record
 gem 'bcrypt',         '3.1.11'
 
+gem 'bootstrap', '~> 4.0.0.beta'
+
 gem 'sqlite3'
 # Use Puma as the app server
 gem 'puma', '~> 3.7'
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
-  get 'posts/new'
+  get 'static_pages/new'
 
-  root 'static_pages#home'
+ root 'static_pages#home'
   
   get '/signup', to: 'users#new'
   post '/signup', to: 'users#create'
@@ -12,6 +12,7 @@ Rails.application.routes.draw do
 
   resources :users
   resources :posts
+
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
